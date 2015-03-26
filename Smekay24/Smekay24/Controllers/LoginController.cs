0457f4218c28c4f7b0f81a32e838a219f61a912f
turambar <alexey.bespalov.2014@gmail.com>
2015-03-26 23:07:50 +0300

    login page added

---
+++ Smekay24/Smekay24/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Smekay24.WebAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Smekay24.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserData user)
+        {
+            SmekayEntities db = new SmekayEntities();
+            
+            if (db.Users.Where(x=>x.Name.Equals(user.Name) && x.Password.Equals(user.Password)).Any())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+                return View();
+        }
+
+    }
+}
---
+++ Smekay24/Smekay24/WebAPI/UserSession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Smekay24.WebAPI
+{
+    public class UserData
+    {
+        public string Name { get; set; }
+
+        public string Password {get;set;}
+
+        public UserData()
+        {
+            Name = "empty";
+            Password = "default";
+        }
+    }
+    public class UserSession
+    {
+        
+    }
+}
