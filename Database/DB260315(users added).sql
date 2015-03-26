98089ea89dc43d596c8981f3c11cb3e318ca4f82
turambar <alexey.bespalov.2014@gmail.com>
2015-03-26 23:12:31 +0300

    test users added sql

---
+++ Database/DB260315(users added).sql
@@ -0,0 +1,5 @@
+insert into Users(Name,Password) values ('admin','admin');
+insert into Users(Name,Password) values ('olga','olga');
+insert into Users(Name,Password) values ('smekay','smekay');
+insert into Users(Name,Password) values ('tarantool','vafla');
+
