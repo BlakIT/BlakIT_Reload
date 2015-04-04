e241dfc91da94f43352443d337e548eb0aad9ed9
tarantool <tarantool_17@inbox.ru>
2015-04-04 22:03:03 +0200

    123

---
+++ Smekay24/Smekay24/Controllers/AdvertController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Smekay24.Controllers
+{
+    public class AdvertController : Controller
+    {
+        //
+        // GET: /Advert/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAdvertView()
+        {
+            return View();
+        }
+
+        public ActionResult AddAdvert()
+        {
+            return View();
+        }
+
+    }
+}
