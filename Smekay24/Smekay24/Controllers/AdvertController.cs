9aa89486e5b52302f515cb7815db15b153084ec0
tarantool <tarantool_17@inbox.ru>
2015-04-06 01:31:13 +0200

    add advert

--- Smekay24/Smekay24/Controllers/AdvertController.cs
+++ Smekay24/Smekay24/Controllers/AdvertController.cs
@@ -8,8 +8,7 @@ namespace Smekay24.Controllers
 {
     public class AdvertController : Controller
     {
-        //
-        // GET: /Advert/
+        private SmekayEntities db = new SmekayEntities();
 
         public ActionResult Index()
         {
@@ -23,7 +22,29 @@ namespace Smekay24.Controllers
 
         public ActionResult AddAdvert()
         {
+
+            ViewData["cities"] = db.City.Select(x => new SelectListItem() { Text = x.Name, Value = x.CCode.ToString() }).ToList();
+            ViewData["category"] = db.Advert_Category.Select(x => new SelectListItem() { Text = x.Name, Value = x.ACCode.ToString() }).ToList(); 
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAdvert(Advert advert, IEnumerable<HttpPostedFileBase> fileUpload)
+        {
+            foreach (var img in fileUpload)
+            {
+                if (img != null)
+                {
+                    string fileName = System.IO.Path.GetFileName(img.FileName);
+                    img.SaveAs(Server.MapPath("~/Files/" + fileName));
+                }
+            }
+
+            db.Advert.Add(advert);
+            db.SaveChanges();
+            
+            return RedirectToAction("Index", "Home");
         }
 
     }
